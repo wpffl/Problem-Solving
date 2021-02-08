5d2ba145fc0721589009ee37da5b2cc1939a7fe2
wpffl <wpffl3333@naver.com>
2021-02-08 16:43:31 +0900

    Create Baekjoon (20299) 3대 측정.java

---
+++ Baekjoon/Baekjoon (20299) 3대 측정.java
@@ -0,0 +1,35 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder(); 
+		StringTokenizer st = new StringTokenizer(br.readLine(), " "); 
+
+		int N = Integer.parseInt(st.nextToken());
+		int S = Integer.parseInt(st.nextToken()); 
+		int M = Integer.parseInt(st.nextToken()); 
+		int res = 0; 
+
+		for (int i = 0; i < N; i++) { 
+			st = new StringTokenizer(br.readLine(), " ");
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken()); 
+			int c = Integer.parseInt(st.nextToken());
+
+			if (st1 >= M && st2 >= M && st3 >= M) { 
+				if (st1 + st2 + st3 >= S) { 
+					res++; 
+					sb.append(a + " " + b + " " + c + " "); 
+				}
+			}
+		}
+		
+		// 최종 출력
+		System.out.println(res);
+		System.out.println(sb.toString());
+	}
+}
