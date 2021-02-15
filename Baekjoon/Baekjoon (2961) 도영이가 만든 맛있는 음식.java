478021bbd346fbebe504b27ad3478c3833b29614
wpffl <wpffl3333@naver.com>
2021-02-15 17:18:32 +0900

    Update Baekjoon (2961) 도영이가 만든 맛있는 음식.java

--- Baekjoon/Baekjoon (2961) 도영이가 만든 맛있는 음식.java
+++ Baekjoon/Baekjoon (2961) 도영이가 만든 맛있는 음식.java
@@ -21,7 +21,6 @@ public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
 
 		N = Integer.parseInt(br.readLine());
 		f = new Flavor[N];
