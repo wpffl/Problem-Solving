f4e4df7b6ac00cbfd54669c9773e92534e7ad5c2
강 윤 정 <wpffl3333@naver.com>
2021-07-26 22:27:36 +0900

    그리디(문자열)

--- Baekjoon/Baekjoon (1464) 뒤집기 3.java
+++ Baekjoon/Baekjoon (1464) 뒤집기 3.java
@@ -3,7 +3,6 @@ import java.io.BufferedWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.StringTokenizer;
 
 public class Main {
 	public static void main(String[] args) throws IOException {
@@ -16,15 +15,18 @@ public class Main {
 		change += origin.charAt(0);
 		
 		for (int i = 1; i < len; i++) {
-			if(change.charAt(i - 1) < origin.charAt(i)) 
-				change = origin.charAt(i) + change;
+			if(change.charAt(i - 1) < origin.charAt(i)) // 다음 문자가 사전순으로 더 큰 경우
+				change = origin.charAt(i) + change; 
 			else
 				change = change + origin.charAt(i);
 		}
 		
+		// 출력
+		// 사전순으로 작은 문자를 뒤로 보냈으므로 출력도 한번 reverse!!
 		for (int i = len - 1; i >= 0; i--) {
 			bw.write(change.charAt(i));
 		}
+		
 		bw.flush();
 	}
 }
